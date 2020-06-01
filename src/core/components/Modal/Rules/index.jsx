9d8fa27596fea9a985e2f5d5eec79d00f3059455
stefanielibarinozup <stefanie.libarino@zup.com.br>
2020-05-31 21:54:13 -0300

    Ajuste no modal de regra do jogo

--- src/core/components/Modal/Rules/index.jsx
+++ src/core/components/Modal/Rules/index.jsx
@@ -9,10 +9,10 @@ const Rules = () => {
     <div className="game">
         <div className="rules">
          <Text.h1 text="Game Rules"/>
-         <Text.h3 text="* To drop a card you need to spend an amount of mana;
-         * When starting the game you will have 20 mana points and 20 life points;
-         * At the end of each round, you recover 2 mana points;
-         * Passing is allowed if you want to save mana for the next round." />
+         <Text.h3 text=" * To release a card the player needs to spend an amount of mana;
+          * When starting the game, the player will have 20 mana points and 20 life points;
+          * At the end of each round, the player and the bug recover 2 mana points and win 1 more card;
+          * Passing is allowed if you want to save mana for the next round." />
          <button className="next">Next</button>
        </div>
     </div>
